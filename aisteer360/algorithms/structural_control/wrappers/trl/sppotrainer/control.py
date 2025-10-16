25841e8b096c93d4435f8852cd240b00852e94f3
Erik Miehling <emiehling@users.noreply.github.com>
2025-10-16 23:59:27 +0100

    structural control refactor

--- aisteer360/algorithms/structural_control/wrappers/trl/sppotrainer/control.py
+++ aisteer360/algorithms/structural_control/wrappers/trl/sppotrainer/control.py
@@ -1,13 +1,9 @@
-from aisteer360.algorithms.structural_control.wrappers.trl.sppotrainer.args import (
-    SPPOArgs,
-)
-from aisteer360.algorithms.structural_control.wrappers.trl.sppotrainer.base_mixin import (
-    SPPOTrainerMixin,
-)
+from aisteer360.algorithms.structural_control.wrappers.trl.sppotrainer.args import SPPOArgs
+from aisteer360.algorithms.structural_control.wrappers.trl.sppotrainer.base_mixin import SPPOTrainerMixin
 
 
 class SPPO(SPPOTrainerMixin):
     """
-
+    SPPO controller.
     """
     Args = SPPOArgs
--- aisteer360/algorithms/structural_control/wrappers/trl/sppotrainer/trainer.py
+++ aisteer360/algorithms/structural_control/wrappers/trl/sppotrainer/trainer.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python
-#
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright (c) 2025 The SPPO Authors and contributors
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+This file includes adapted portions of code from https://github.com/uclaml/SPPO (Apache License 2.0).
+Modifications by IBM Research, 2025: refactoring, integration, bug fixes, and comments.
+"""
+
 # Adapted from https://github.com/huggingface/alignment-handbook
 
 # From https://github.com/uclaml/SPPO/blob/main/sppo/trainer.py
