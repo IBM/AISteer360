8cf598b57a49ca80937aae5a9093a3ceaef300a2
cyko@ibm.com;6J3007897;Irene Ko <cyko1@ccc-login5.pok.ibm.com>
2026-04-07 15:53:53 -0400

    Added dependency; remove dummy absolute path
    Signed-off-by: cyko@ibm.com;6J3007897;Irene Ko <cyko1@ccc-login5.pok.ibm.com>

--- test_vllm_state.py
+++ test_vllm_state.py
@@ -1,6 +1,6 @@
 import multiprocessing as mp
 
-MODEL = "/dccstor/pyrite/irene/models--meta-llama--Llama-3.1-8B-Instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659"
+MODEL = ""
 
 
 def main():
